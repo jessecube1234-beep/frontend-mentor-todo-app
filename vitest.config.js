974a1f1c18ec21d295ea1cf1aed0fe79f78fb3aa
Jesse <jessecube1234@gmail.com>
2026-01-16 07:20:53 -0600

    implement fix for the toggle tests

--- vitest.config.js
+++ vitest.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
       '@utils': path.resolve(__dirname, './src/utils'),
       '@providers': path.resolve(__dirname, './src/components/providers'),
       '@test': path.resolve(__dirname, './test'),
+      '@footers': path.resolve(__dirname, './src/components/layout/footers'),
     },
   },
   test: {

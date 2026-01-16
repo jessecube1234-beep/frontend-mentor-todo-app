c22d13f2cf75a4b17b0d4f83546f8eb94b507a07
Jesse <jessecube1234@gmail.com>
2026-01-16 08:02:25 -0600

    add test for the useTasks hook

--- test/components/Header.test.jsx
+++ test/components/Header.test.jsx
@@ -8,7 +8,7 @@
  */
 import React from "react";
 import { expect, test, describe } from 'vitest';
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import Header from "@components/layout/Header";
